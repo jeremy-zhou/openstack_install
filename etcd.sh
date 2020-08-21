edcf3b184b8c475733177c371b96c42ebfc5b72a
root <feng4206yu@163.com>
2020-08-21 15:47:31 +0800

    fix etcd conf

--- etcd.sh
+++ etcd.sh
@@ -2,16 +2,18 @@
 
 yum install etcd -y
 
-crudini --set /etc/etcd/etcd.conf '' ETCD_DATA_DIR "/var/lib/etcd/default.etcd"
-crudini --set /etc/etcd/etcd.conf '' ETCD_LISTEN_PEER_URLS "http://192.168.2.246:2380"
-crudini --set /etc/etcd/etcd.conf '' ETCD_LISTEN_CLIENT_URLS "http://192.168.2.246:2379"
-crudini --set /etc/etcd/etcd.conf '' ETCD_NAME "controller"
+crudini --set /etc/etcd/etcd.conf '' ETCD_DATA_DIR "\"/var/lib/etcd/default.etcd\""
+#crudini --set /etc/etcd/etcd.conf '' ETCD_LISTEN_PEER_URLS "http://192.168.2.246:2380"
+sed -i "s/^#ETCD_LISTEN_PEER_URLS.\+$/ETCD_LISTEN_PEER_URLS=\"http:\/\/192.168.2.246:2380\"/g" /etc/etcd/etcd.conf
+crudini --set /etc/etcd/etcd.conf '' ETCD_LISTEN_CLIENT_URLS "\"http://192.168.2.246:2379\""
+crudini --set /etc/etcd/etcd.conf '' ETCD_NAME "\"controller\""
 
-crudini --set /etc/etcd/etcd.conf '' ETCD_INITIAL_ADVERTISE_PEER_URLS "http://192.168.2.246:2380"
-crudini --set /etc/etcd/etcd.conf '' ETCD_ADVERTISE_CLIENT_URLS "http://192.168.2.246:2379"
-crudini --set /etc/etcd/etcd.conf '' ETCD_INITIAL_CLUSTER "controller=http://192.168.2.246:2380"
-crudini --set /etc/etcd/etcd.conf '' ETCD_INITIAL_CLUSTER_TOKEN "etcd-cluster-01"
-crudini --set /etc/etcd/etcd.conf '' ETCD_INITIAL_CLUSTER_STATE "new"
+#crudini --set /etc/etcd/etcd.conf '' ETCD_INITIAL_ADVERTISE_PEER_URLS "\"http://192.168.2.246:2380\""
+sed -i "s/^#ETCD_INITIAL_ADVERTISE_PEER_URLS.\+$/ETCD_INITIAL_ADVERTISE_PEER_URLS=\"http:\/\/192.168.2.246:2380\"/g" /etc/etcd/etcd.conf
+crudini --set /etc/etcd/etcd.conf '' ETCD_ADVERTISE_CLIENT_URLS "\"http://192.168.2.246:2379\""
+crudini --set /etc/etcd/etcd.conf '' ETCD_INITIAL_CLUSTER "\"controller=http://192.168.2.246:2380\""
+crudini --set /etc/etcd/etcd.conf '' ETCD_INITIAL_CLUSTER_TOKEN "\"etcd-cluster-01\""
+crudini --set /etc/etcd/etcd.conf '' ETCD_INITIAL_CLUSTER_STATE "\"new\""
 
 systemctl enable --now etcd
 systemctl status etcd
