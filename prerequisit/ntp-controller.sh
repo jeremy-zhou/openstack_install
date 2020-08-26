e7b02776cc35b1e72d78906518d456bbe266ffd6
root <jeremypzhou@outlook.com>
2020-08-26 19:00:53 +0800

    replace with parameters

--- prerequisit/ntp-controller.sh
+++ prerequisit/ntp-controller.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
-yum install chrony -y
+#yum install chrony -y
 sleep 2s
-sed -i "s/0.centos.pool.ntp.org/ntp.aliyun.com/g" /etc/chrony.conf
-sed -i "s/1.centos.pool.ntp.org/ntp2.aliyun.com/g" /etc/chrony.conf
-sed -i "s/2.centos.pool.ntp.org/ntp3.aliyun.com/g" /etc/chrony.conf
-sed -i "s/3.centos.pool.ntp.org/ntp6.aliyun.com/g" /etc/chrony.conf
-sed -i "s/^#allow\s\+192.168.0.0\/16/allow 192.168.0.0\/16/g" /etc/chrony.conf
-systemctl enable --now chronyd.service
-systemctl status chronyd.service
+sed -i "s/0.centos.pool.ntp.org/$1/g" /etc/chrony.conf
+sed -i "s/1.centos.pool.ntp.org/$2/g" /etc/chrony.conf
+sed -i "s/2.centos.pool.ntp.org/$3/g" /etc/chrony.conf
+sed -i "/3.centos.pool.ntp.org/d" /etc/chrony.conf
+sed -i "s/^#allow\s\+192.168.0.0\/16/allow $4\/$5/g" /etc/chrony.conf
+#systemctl enable --now chronyd.service
+#systemctl status chronyd.service
 sleep 2s
 date
  
