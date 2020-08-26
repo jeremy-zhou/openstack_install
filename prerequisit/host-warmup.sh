6a7a382e71163592e82bc57db2d0ba1dcfe593fa
jeremy_zhou <feng4206yu@163.com>
2020-08-26 17:01:42 +0800

    remove aliyuncs in aliyun repo

--- prerequisit/host-warmup.sh
+++ prerequisit/host-warmup.sh
@@ -3,6 +3,7 @@ yum install -y wget vim git
 mkdir /etc/yum.repos.d/bak
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/bak/
 wget -O /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
+sed -i -e '/mirrors.cloud.aliyuncs.com/d' -e '/mirrors.aliyuncs.com/d' /etc/yum.repos.d/CentOS-Base.repo
 yum clean all
 yum makecache
 yum install centos-release-openstack-train -y
