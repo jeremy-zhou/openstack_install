67a4fbc4aa129b5c998c31b2c4d1e88cad560df9
jeremy_zhou <feng4206yu@163.com>
2020-08-26 19:19:10 +0800

    add 4th ntp server

--- prerequisit/ntp-controller.sh
+++ prerequisit/ntp-controller.sh
@@ -4,9 +4,10 @@ sleep 2s
 sed -i "s/0.centos.pool.ntp.org/$1/g" /etc/chrony.conf
 sed -i "s/1.centos.pool.ntp.org/$2/g" /etc/chrony.conf
 sed -i "s/2.centos.pool.ntp.org/$3/g" /etc/chrony.conf
-sed -i "/3.centos.pool.ntp.org/d" /etc/chrony.conf
-sed -i "s/^#allow\s\+192.168.0.0\/16/allow $4\/$5/g" /etc/chrony.conf
-systemctl enable --now chronyd.service
+sed -i "s/3.centos.pool.ntp.org/$4/g" /etc/chrony.conf
+sed -i "s/^#allow\s\+192.168.0.0\/16/allow $5\/$6/g" /etc/chrony.conf
+systemctl enable chronyd.service
+systemctl restart chronyd.service
 systemctl status chronyd.service
 sleep 2s
 date
