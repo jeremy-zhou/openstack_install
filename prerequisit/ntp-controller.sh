324a11cb04d5521134dcfac713481fc7051b8c90
jeremy_zhou <feng4206yu@163.com>
2020-08-26 16:23:02 +0800

    replace ntp.sjtu.edu.cn server with another aliyun ntp server

--- prerequisit/ntp-controller.sh
+++ prerequisit/ntp-controller.sh
@@ -3,11 +3,12 @@
 yum install chrony -y
 sleep 2s
 sed -i "s/0.centos.pool.ntp.org/ntp.aliyun.com/g" /etc/chrony.conf
-sed -i "s/1.centos.pool.ntp.org/ntp.sjtu.edu.cn/g" /etc/chrony.conf
-sed -i "s/2.centos.pool.ntp.org/ntp6.aliyun.com/g" /etc/chrony.conf
-sed -i "s/3.centos.pool.ntp.org/ntp3.aliyun.com/g" /etc/chrony.conf
+sed -i "s/1.centos.pool.ntp.org/ntp2.aliyun.com/g" /etc/chrony.conf
+sed -i "s/2.centos.pool.ntp.org/ntp3.aliyun.com/g" /etc/chrony.conf
+sed -i "s/3.centos.pool.ntp.org/ntp6.aliyun.com/g" /etc/chrony.conf
 sed -i "s/^#allow\s\+192.168.0.0\/16/allow 192.168.0.0\/16/g" /etc/chrony.conf
 systemctl enable --now chronyd.service
 systemctl status chronyd.service
+sleep 2s
 date
  
