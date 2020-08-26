b994e8b5251a061dfbd4666a877721967f36d120
jeremy_zhou <feng4206yu@163.com>
2020-08-26 19:02:01 +0800

    Update ntp-controller.sh

--- prerequisit/ntp-controller.sh
+++ prerequisit/ntp-controller.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#yum install chrony -y
+yum install chrony -y
 sleep 2s
 sed -i "s/0.centos.pool.ntp.org/$1/g" /etc/chrony.conf
 sed -i "s/1.centos.pool.ntp.org/$2/g" /etc/chrony.conf
 sed -i "s/2.centos.pool.ntp.org/$3/g" /etc/chrony.conf
 sed -i "/3.centos.pool.ntp.org/d" /etc/chrony.conf
 sed -i "s/^#allow\s\+192.168.0.0\/16/allow $4\/$5/g" /etc/chrony.conf
-#systemctl enable --now chronyd.service
-#systemctl status chronyd.service
+systemctl enable --now chronyd.service
+systemctl status chronyd.service
 sleep 2s
 date
  
